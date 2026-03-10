0c4ee5f57055834ee547b16031e6458a7952c4cc
Mintbeast7 <bhattacharjeebidyut07@gmail.com>
2026-03-10 22:45:21 +0530

    Removed eclipse metadata from repo

--- order-service/src/main/java/com/commerce/productclient/ProductClient.java
+++ order-service/src/main/java/com/commerce/productclient/ProductClient.java
@@ -5,12 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.commerce.dto.ProductResponse;
 
-@FeignClient(name = "product-service") //configuration = TracingFeignConfig.class)
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+
+@FeignClient(name = "product-service") // configuration = TracingFeignConfig.class)
 public interface ProductClient {
 
-    @GetMapping("/products/{id}")
-    ProductResponse getProductById(@PathVariable("id") Long id);
-}
+	@CircuitBreaker(name = "ProductService", fallbackMethod = "productFallback")
+	@Retry(name = "ProductServiceRetry")
+	@GetMapping("/products/{id}")
+	ProductResponse getProductById(@PathVariable("id") Long id);
 
+	// fallback method
+	default ProductResponse productFallback(Long id, Exception e) {
+
+		ProductResponse response = new ProductResponse();
+		response.setProductId(id);
+		response.setProductName("Product service is currently unavailable. Please try again later.");
+		response.setPrice(0.0);
+
+		return response;
+	}
+}
 
 //This code lets Order Service call Product Service’s /products/{id} API and receive product data as a ProductResponse object.
